8c64b4bd81c3a617f84dec153b226f34585e3742
Your Name <you@example.com>
2021-11-14 23:00:28 -0300

    adding books form

--- frontend/src/Pages/AddBook.js
+++ frontend/src/Pages/AddBook.js
@@ -1,13 +1,9 @@
 import React, {useEffect, useState} from 'react';
 import NavbarMain from "../Components/NavbarMain";
 import {
-    Box,
-    Container,
-    Grid,
-    Table, TableBody,
-    TableCell,
-    TableContainer, TableHead,
-    TableRow,
+    Box, Button,
+    Container, FormControl,
+    Grid, Table, TableContainer, TableHead, TableRow, TextareaAutosize,
     TextField,
     Typography
 } from "@material-ui/core";
@@ -22,15 +18,10 @@ export default() => {
     const navigate = useNavigate();
 
     const [user, setUser] = useState([]);
+    const [logged, setLogged] = useState();
 
     useEffect(() => {
-
-        console.log("ok")
-
-    }, []);
-
-    function auth() {
-        // verify token
+        // --------------------------------------- verify token
         ApiService().get("/users/me", {
             headers: {
                 'Authorization': `Bearer ${Cookies.get("token")}`,
@@ -38,31 +29,100 @@ export default() => {
             }
         }).then(response => {
             setUser(response.data);
+            setLogged(true);
         }).catch(err => {
             setUser(null);
+            setLogged(false);
         })
 
-        if(user == null) {
-            navigate("/");
+    }, []);
+
+
+
+    // add new book
+    const [title, setTitle] = useState();
+    const [author, setAuthor] = useState();
+    const [description, setDescription] = useState();
+    const [year, setYear] = useState();
+    const [price, setPrice] = useState();
+
+    function addBook(e) {
+        e.preventDefault();
+
+        if(!title || !description || !year || price) {
+            console.log("an error occurred");
+        }else {
+            ApiService().post("/books/new", {
+                headers: {
+                    'Authorization': `Bearer ${Cookies.get("token")}`,
+                    'Access-Control-Allow-Origin': '*'
+                }
+            }, {
+                title, description, author, year, price
+            }).then(response => {
+                console.log(response);
+            }).catch(err => {
+                console.log(err);
+            })
         }
-    }auth();
+    }
 
-    return(
-        <>
-            <NavbarMain />
-            <Box mt={18}>
-                <Container>
-                    <Grid item md={12}>
-                        <Typography variant={"h5"}>Add new book</Typography>
-                        <hr />
-                    </Grid>
-                    <Grid item>
-                        <Typography>You can add new book behind the form...</Typography>
-                    </Grid>
-                </Container>
-            </Box>
-        </>
-    );
+    if(!logged) {
+        return <Typography>You don't have permission.</Typography>
+    }else{
+        return(
+            <>
+                <NavbarMain />
+                <Box mt={18}>
+                    <Container>
+                        <Grid container spacing={2}>
+                            <Grid item md={12}>
+                                <Typography variant={"h5"}>Add new book</Typography>
+                                <hr />
+                            </Grid>
+                            <Grid item md={12}>
+                                <Typography>You can add new book below in the form.</Typography>
+                            </Grid>
+                            <Grid item md={12}>
+                                <form onSubmit={addBook}>
+                                    <TableContainer>
+                                        <Table>
+                                            <TableHead>
+                                                <TableRow>
+                                                    <TextField label={"Title"} value={title} onChange={e => setTitle(e.target.value)} style={{width: '100%'}}></TextField>
+                                                </TableRow>
+                                                <TableRow>
+                                                    <TextField label={"Author"} value={author} onChange={e => setAuthor(e.target.value)} style ={{width: '100%'}}></TextField>
+                                                </TableRow>
+                                                <TableRow>
+                                                    <br/>
+                                                    <Typography>Description:</Typography>
+                                                    <TextareaAutosize
+                                                        value={description} onChange={e => setDescription(e.target.value)}
+                                                        placeholder="max 200 characters"
+                                                        maxLength={200}
+                                                        style={{width: "-webkit-fill-available", minHeight:"100px", marginTop:"10px", padding:"10px"}}
+                                                    />
+                                                </TableRow>
+                                                <TableRow>
+                                                    <TextField label={"Year"} value={year} onChange={e => setYear(e.target.value)} style={{width: '100%'}}></TextField>
+                                                </TableRow>
+                                                <TableRow>
+                                                    <TextField label={"Price"} value={price} onChange={e => setPrice(e.target.value)} style={{width: '100%'}}></TextField>
+                                                </TableRow>
+                                                <TableRow>
+                                                    <Button variant="contained" style={{background:"#8BC34A", color:"#fff", marginTop:"25px"}}>Insert book</Button>
+                                                </TableRow>
+                                            </TableHead>
+                                        </Table>
+                                    </TableContainer>
+                                </form>
+                            </Grid>
+                        </Grid>
+                    </Container>
+                </Box>
+            </>
+        );
 
-
+    }
 }
--- frontend/src/Pages/Main.js
+++ frontend/src/Pages/Main.js
@@ -15,21 +15,18 @@ import Cookies from 'js-cookie';
 
 import ApiService from '../Service/ApiService';
 import {Alert} from "@mui/material";
-import {useNavigate} from "react-router-dom";
+import {Navigate, useNavigate} from "react-router-dom";
 
 export default() => {
 
     const navigate = useNavigate();
 
     const [user, setUser] = useState([]);
+    const [logged, setLogged] = useState();
 
     useEffect(() => {
-        findBooks();
 
-    }, []);
-
-    function auth() {
-        // verify token
+        // --------------------------------------- verify token
         ApiService().get("/users/me", {
             headers: {
                 'Authorization': `Bearer ${Cookies.get("token")}`,
@@ -37,14 +34,17 @@ export default() => {
             }
         }).then(response => {
             setUser(response.data);
+            setLogged(true);
         }).catch(err => {
             setUser(null);
+            setLogged(false);
         })
 
-        if(user == null) {
-            navigate("/");
-        }
-    }auth();
+        findBooks();
+
+    }, []);
+
+
 
     const [message, setMessage] = useState("");
     const [messageType, setMessageType] = useState("");
@@ -97,89 +97,93 @@ export default() => {
 
     }
 
-    return(
-        <>
-            <NavbarMain />
-            <Box mt={18}>
-                <Container>
-                    <Grid container spacing={2} justify="space-between">
-                        <Grid item md={12}>
-                            <Typography>Welcome, {user.name}</Typography>
-                            <br />
-                        </Grid>
-                        <Grid item md={10}>
-                            <Typography variant={"h5"}>Book List</Typography>
-                        </Grid>
-                        <Grid item md={2}>
-                            <TextField sx={{float: "right"}} id="outlined-basic" label="Search book" value={search} onChange={e => {setSearch(e.target.value)}} onKeyUp={e => {
-                                if(e.code == 'Enter') {
-                                    doSearch();
+    if(!logged) {
+        return <Typography>You don't have permission.</Typography>
+    } else{
+        return(
+            <>
+                <NavbarMain />
+                <Box mt={18}>
+                    <Container>
+                        <Grid container spacing={2} justify="space-between">
+                            <Grid item md={12}>
+                                <Typography>Welcome, {user.name}</Typography>
+                                <br />
+                            </Grid>
+                            <Grid item md={10}>
+                                <Typography variant={"h5"}>Book List</Typography>
+                            </Grid>
+                            <Grid item md={2}>
+                                <TextField sx={{float: "right"}} id="outlined-basic" label="Search book" value={search} onChange={e => {setSearch(e.target.value)}} onKeyUp={e => {
+                                    if(e.code == 'Enter') {
+                                        doSearch();
+                                    }
+                                }} variant="outlined" />
+                            </Grid>
+
+                            <Grid item md={12}>
+                                {(searchedBooks.length >= 1) &&
+                                <>
+                                    <Typography>Found {searchedBooks.length} results!</Typography>
+                                    <hr/>
+                                    <TableContainer>
+                                        <Table>
+                                            <TableHead>
+                                                <TableRow>
+                                                    <TableCell>
+                                                        <Typography variant={"h6"}>Title</Typography>
+                                                    </TableCell>
+                                                    <TableCell>
+                                                        <Typography variant={"h6"}>Description</Typography>
+                                                    </TableCell>
+                                                    <TableCell>
+                                                        <Typography variant={"h6"}>Author</Typography>
+                                                    </TableCell>
+                                                    <TableCell>
+                                                        <Typography variant={"h6"}>Year</Typography>
+                                                    </TableCell>
+                                                    <TableCell>
+                                                        <Typography variant={"h6"}>Price</Typography>
+                                                    </TableCell>
+                                                </TableRow>
+                                            </TableHead>
+
+                                            <TableBody>
+                                                {(searchedBooks).map(book => {
+                                                    return(
+                                                        <TableRow>
+                                                            <TableCell>
+                                                                {book.title}
+                                                            </TableCell>
+                                                            <TableCell>
+                                                                {book.description}
+                                                            </TableCell>
+                                                            <TableCell>
+                                                                {(book.author) ? <>{book.author}</> : <i>Unknown</i> }
+                                                            </TableCell>
+                                                            <TableCell>
+                                                                {book.releaseYear}
+                                                            </TableCell>
+                                                            <TableCell>
+                                                                $ {book.price}
+                                                            </TableCell>
+                                                        </TableRow>
+                                                    )
+                                                })
+                                                }
+                                            </TableBody>
+
+                                        </Table>
+                                    </TableContainer>
+                                </>
                                 }
-                            }} variant="outlined" />
+                            </Grid>
                         </Grid>
-
-                        <Grid item md={12}>
-                            {(searchedBooks.length >= 1) &&
-                            <>
-                                <Typography>Found {searchedBooks.length} results!</Typography>
-                                <hr/>
-                                <TableContainer>
-                                    <Table>
-                                        <TableHead>
-                                            <TableRow>
-                                                <TableCell>
-                                                    <Typography variant={"h6"}>Title</Typography>
-                                                </TableCell>
-                                                <TableCell>
-                                                    <Typography variant={"h6"}>Description</Typography>
-                                                </TableCell>
-                                                <TableCell>
-                                                    <Typography variant={"h6"}>Author</Typography>
-                                                </TableCell>
-                                                <TableCell>
-                                                    <Typography variant={"h6"}>Year</Typography>
-                                                </TableCell>
-                                                <TableCell>
-                                                    <Typography variant={"h6"}>Price</Typography>
-                                                </TableCell>
-                                            </TableRow>
-                                        </TableHead>
-
-                                        <TableBody>
-                                            {(searchedBooks).map(book => {
-                                                return(
-                                                    <TableRow>
-                                                        <TableCell>
-                                                            {book.title}
-                                                        </TableCell>
-                                                        <TableCell>
-                                                            {book.description}
-                                                        </TableCell>
-                                                        <TableCell>
-                                                            {(book.author) ? <>{book.author}</> : <i>Unknown</i> }
-                                                        </TableCell>
-                                                        <TableCell>
-                                                            {book.releaseYear}
-                                                        </TableCell>
-                                                        <TableCell>
-                                                            $ {book.price}
-                                                        </TableCell>
-                                                    </TableRow>
-                                                )
-                                            })
-                                            }
-                                        </TableBody>
-
-                                    </Table>
-                                </TableContainer>
-                            </>
-                            }
-                        </Grid>
-                    </Grid>
-                </Container>
-            </Box>
-        </>
-    )
+                    </Container>
+                </Box>
+            </>
+        )
+    }
 
 
 }
