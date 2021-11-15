add741b288bdaa6aa580fc8497ae893b5f7a9d9a
Your Name <you@example.com>
2021-11-14 21:40:06 -0300

    adding new books initializing

--- frontend/src/Pages/AddBook.js
+++ frontend/src/Pages/AddBook.js
@@ -15,16 +15,21 @@ import Cookies from 'js-cookie';
 
 import ApiService from '../Service/ApiService';
 import {Alert} from "@mui/material";
+import {useNavigate} from "react-router-dom";
 
 export default() => {
 
-    const auth = useAuth();
+    const navigate = useNavigate();
 
-    const [email, setEmail] = useState('');
-    const [name, setName] = useState('');
+    const [user, setUser] = useState([]);
 
     useEffect(() => {
 
+        console.log("ok")
+
+    }, []);
+
+    function auth() {
         // verify token
         ApiService().get("/users/me", {
             headers: {
@@ -32,18 +37,30 @@ export default() => {
                 'Access-Control-Allow-Origin': '*'
             }
         }).then(response => {
-            setEmail(response.data.email);
-            setName(response.data.name);
+            setUser(response.data);
         }).catch(err => {
-            console.log(err)
+            setUser(null);
         })
 
-    }, []);
+        if(user == null) {
+            navigate("/");
+        }
+    }auth();
 
     return(
         <>
             <NavbarMain />
-            Add new book
+            <Box mt={18}>
+                <Container>
+                    <Grid item md={12}>
+                        <Typography variant={"h5"}>Add new book</Typography>
+                        <hr />
+                    </Grid>
+                    <Grid item>
+                        <Typography>You can add new book behind the form...</Typography>
+                    </Grid>
+                </Container>
+            </Box>
         </>
     );
 
